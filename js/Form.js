a623ec6ca5e15c1c0ae3ec1d46ecb397e8554a43
Agrim976 <73536150+Agrim976@users.noreply.github.com>
2021-03-02 15:38:36 +0530

    Add files via upload

---
+++ js/Form.js
@@ -0,0 +1,35 @@
+class Form {
+
+  constructor() {
+    this.input = createInput("Name");
+    this.button = createButton('Play');
+    this.greeting = createElement('h2');
+  }
+  hide(){
+    this.greeting.hide();
+    this.button.hide();
+    this.input.hide();
+  }
+
+  display(){
+    var title = createElement('h2')
+    title.html("Car Racing Game");
+    title.position(displayWidth/2-50, 0);
+
+    this.input.position(displayWidth/2-40, displayHeight/2-80);
+    this.button.position(displayWidth/2+30, displayHeight/2);
+
+    this.button.mousePressed(()=>{//=> is another way of writing function and is called the arrow function and it binds or refers the function to the class directly and not to the object
+      this.input.hide();
+      this.button.hide();
+      player.name = this.input.value();
+      playerCount+=1;
+      player.index = playerCount;
+      player.update();
+      player.updateCount(playerCount);
+      this.greeting.html("Hello " + player.name)
+      this.greeting.position(displayWidth/2-70, displayHeight/4);
+    });
+
+  }
+}
